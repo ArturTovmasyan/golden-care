7b136640f46a224711bd9ae809756a9ca118d352
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2019-12-25 14:43:11 +0400

    #837 - "Type" for Assessment

---
+++ src/Repository/Assessment/AssessmentTypeRepository.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace App\Repository\Assessment;
+
+use App\Api\V1\Component\RelatedInfoInterface;
+use App\Entity\Assessment\AssessmentType;
+use App\Entity\Space;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Class AssessmentTypeRepository
+ * @package App\Repository\Assessment
+ */
+class AssessmentTypeRepository extends EntityRepository implements RelatedInfoInterface
+{
+    /**
+     * @param Space|null $space
+     * @param array|null $entityGrants
+     * @param QueryBuilder $queryBuilder
+     */
+    public function search(Space $space = null, array $entityGrants = null, QueryBuilder $queryBuilder) : void
+    {
+        $queryBuilder
+            ->from(AssessmentType::class, 'at')
+            ->innerJoin(
+                Space::class,
+                's',
+                Join::WITH,
+                's = at.space'
+            );
+
+        if ($space !== null) {
+            $queryBuilder
+                ->andWhere('s = :space')
+                ->setParameter('space', $space);
+        }
+
+        if ($entityGrants !== null) {
+            $queryBuilder
+                ->andWhere('at.id IN (:grantIds)')
+                ->setParameter('grantIds', $entityGrants);
+        }
+
+        $queryBuilder
+            ->groupBy('at.id');
+    }
+
+    /**
+     * @param Space|null $space
+     * @param array|null $entityGrants
+     * @return mixed
+     */
+    public function list(Space $space = null, array $entityGrants = null)
+    {
+        $qb = $this
+            ->createQueryBuilder('at')
+            ->innerJoin(
+                Space::class,
+                's',
+                Join::WITH,
+                's = at.space'
+            );
+
+        if ($space !== null) {
+            $qb
+                ->andWhere('s = :space')
+                ->setParameter('space', $space);
+        }
+
+        if ($entityGrants !== null) {
+            $qb
+                ->andWhere('at.id IN (:grantIds)')
+                ->setParameter('grantIds', $entityGrants);
+        }
+
+        $qb
+            ->addOrderBy('at.title', 'ASC');
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @param Space|null $space
+     * @param array|null $entityGrants
+     * @param $id
+     * @return mixed
+     */
+    public function getOne(Space $space = null, array $entityGrants = null, $id)
+    {
+        $qb = $this
+            ->createQueryBuilder('at')
+            ->innerJoin(
+                Space::class,
+                's',
+                Join::WITH,
+                's = at.space'
+            )
+            ->where('at.id = :id')
+            ->setParameter('id', $id);
+
+        if ($space !== null) {
+            $qb
+                ->andWhere('s = :space')
+                ->setParameter('space', $space);
+        }
+
+        if ($entityGrants !== null) {
+            $qb
+                ->andWhere('at.id IN (:grantIds)')
+                ->setParameter('grantIds', $entityGrants);
+        }
+
+        return $qb
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * @param Space|null $space
+     * @param array|null $entityGrants
+     * @param $ids
+     * @return mixed
+     */
+    public function findByIds(Space $space = null, array $entityGrants = null, $ids)
+    {
+        $qb = $this
+            ->createQueryBuilder('at')
+            ->where('at.id IN (:ids)')
+            ->setParameter('ids', $ids);
+
+        if ($space !== null) {
+            $qb
+                ->innerJoin(
+                    Space::class,
+                    's',
+                    Join::WITH,
+                    's = at.space'
+                )
+                ->andWhere('s = :space')
+                ->setParameter('space', $space);
+        }
+
+        if ($entityGrants !== null) {
+            $qb
+                ->andWhere('at.id IN (:grantIds)')
+                ->setParameter('grantIds', $entityGrants);
+        }
+
+        return $qb->groupBy('at.id')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @param Space|null $space
+     * @param array|null $entityGrants
+     * @param null $mappedBy
+     * @param null $id
+     * @param array|null $ids
+     * @return mixed
+     */
+    public function getRelatedData(Space $space = null, array $entityGrants = null, $mappedBy = null, $id = null, array $ids = null)
+    {
+        $qb = $this
+            ->createQueryBuilder('at')
+            ->select('at.title');
+
+        if ($mappedBy !== null && $id !== null) {
+            $qb
+                ->where('at.'.$mappedBy.'= :id')
+                ->setParameter('id', $id);
+        }
+
+        if ($ids !== null) {
+            $qb
+                ->andWhere('at.id IN (:ids)')
+                ->setParameter('ids', $ids);
+        }
+
+        if ($mappedBy === null && $id === null && $ids === null) {
+            $qb
+                ->andWhere('at.id IN (:array)')
+                ->setParameter('array', []);
+        }
+
+        if ($space !== null) {
+            $qb
+                ->innerJoin(
+                    Space::class,
+                    's',
+                    Join::WITH,
+                    's = at.space'
+                )
+                ->andWhere('s = :space')
+                ->setParameter('space', $space);
+        }
+
+        if ($entityGrants !== null) {
+            $qb
+                ->andWhere('at.id IN (:grantIds)')
+                ->setParameter('grantIds', $entityGrants);
+        }
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+}
