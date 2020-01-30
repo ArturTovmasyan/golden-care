b9afdd768ffa28e507fa4e1e2087357d3b6ec275
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2020-01-30 12:38:50 +0400

    #849 - Fix in Payment Source.

--- src/Repository/PaymentSourceRepository.php
+++ src/Repository/PaymentSourceRepository.php
@@ -25,7 +25,7 @@ class PaymentSourceRepository extends EntityRepository implements RelatedInfoInt
     {
         $queryBuilder
             ->from(PaymentSource::class, 'ps')
-            ->addSelect('JSON_ARRAYAGG(JSON_OBJECT(cl.title, br.amount)) AS base_rates')
+            ->addSelect('JSON_ARRAYAGG(CASE WHEN cl.title IS NOT NULL THEN JSON_OBJECT(cl.title, br.amount) ELSE \'\' END) AS base_rates')
             ->innerJoin(
                 Space::class,
                 's',
