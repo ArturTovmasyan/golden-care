5a14aec1bcd2f5622c99e6d15c0b934d405d3c3e
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2020-02-03 20:51:43 +0400

    #875 - Fix in Room report.

--- src/Repository/ResidentRentRepository.php
+++ src/Repository/ResidentRentRepository.php
@@ -781,12 +781,12 @@ class ResidentRentRepository extends EntityRepository implements RelatedInfoInte
     {
         $qb = $this
             ->getResidentAdmissionWithRentQb($type, $reportInterval, $typeId)
-            ->andWhere('rr.id IN (SELECT MAX(mrr.id) 
-                        FROM App:ResidentRent mrr 
-                        JOIN mrr.resident res 
-                        WHERE (mrr.end IS NULL OR mrr.end > = ra.start) AND (ra.end IS NULL OR mrr.start < = ra.end)
-                        GROUP BY res.id)'
-            )
+//            ->andWhere('rr.id IN (SELECT MAX(mrr.id)
+//                        FROM App:ResidentRent mrr
+//                        JOIN mrr.resident res
+//                        WHERE (mrr.end IS NULL OR mrr.end > = ra.start) AND (ra.end IS NULL OR mrr.start < = ra.end)
+//                        GROUP BY res.id)'
+//            )
             ->andWhere('r.id IN (SELECT ar.id 
                         FROM App:ResidentAdmission ara 
                         JOIN ara.resident ar 
