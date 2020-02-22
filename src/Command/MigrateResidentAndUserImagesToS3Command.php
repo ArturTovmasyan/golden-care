1509383597b1b3d12557579b4b6b82d888d16c43
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2020-02-22 11:15:52 +0400

    #655 - Changed resident and user images command for migration to S3.

--- src/Command/MigrateResidentAndUserImagesToS3Command.php
+++ src/Command/MigrateResidentAndUserImagesToS3Command.php
@@ -3,7 +3,6 @@
 namespace App\Command;
 
 use App\Api\V1\Common\Service\Exception\ValidationException;
-use App\Api\V1\Common\Service\ImageFilterService;
 use App\Api\V1\Common\Service\S3Service;
 use App\Entity\Image;
 use App\Entity\ResidentImage;
@@ -12,10 +11,13 @@ use App\Model\FileType;
 use App\Util\MimeUtil;
 use DataURI\Parser;
 use Doctrine\ORM\EntityManagerInterface;
+use Liip\ImagineBundle\Imagine\Filter\FilterManager;
+use Liip\ImagineBundle\Model\Binary;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class MigrateResidentAndUserImagesToS3Command extends Command
 {
@@ -32,22 +34,22 @@ class MigrateResidentAndUserImagesToS3Command extends Command
     protected $s3Service;
 
     /**
-     * @var ImageFilterService
+     * @var ContainerInterface
      */
-    protected $imageFilterService;
+    protected $container;
 
     /**
      * InviteCustomerCommand constructor.
      * @param EntityManagerInterface $em
      * @param S3Service $s3Service
-     * @param ImageFilterService $imageFilterService
+     * @param ContainerInterface $container
      */
-    public function __construct(EntityManagerInterface $em, S3Service $s3Service, ImageFilterService $imageFilterService)
+    public function __construct(EntityManagerInterface $em, S3Service $s3Service, ContainerInterface $container)
     {
         parent::__construct();
         $this->em = $em;
         $this->s3Service = $s3Service;
-        $this->imageFilterService = $imageFilterService;
+        $this->container = $container;
     }
 
     /**
@@ -109,7 +111,7 @@ class MigrateResidentAndUserImagesToS3Command extends Command
 
                         $this->s3Service->uploadFile($photo, $s3Id, $image->getType(), $image->getMimeType());
 
-                        $this->imageFilterService->createAllFilterVersion($image, $base64Image, $mimeType, $format);
+                        $this->createAllFilterVersion($image, $base64Image, $mimeType, $format);
                     }
                 }
             }
@@ -146,7 +148,7 @@ class MigrateResidentAndUserImagesToS3Command extends Command
 
                         $this->s3Service->uploadFile($photo, $s3Id, $image->getType(), $image->getMimeType());
 
-                        $this->imageFilterService->createAllFilterVersion($image, $base64Image, $mimeType, $format);
+                        $this->createAllFilterVersion($image, $base64Image, $mimeType, $format);
                     }
                 }
             }
@@ -169,5 +171,54 @@ class MigrateResidentAndUserImagesToS3Command extends Command
         $this->release();
 
         return 1;
+    }
+
+    /**
+     * @param Image $image
+     * @param $base64Image
+     * @param $mimeType
+     * @param $format
+     */
+    public function createAllFilterVersion($image, $base64Image, $mimeType, $format): void
+    {
+        //create binary
+        $binary = new Binary($base64Image, $mimeType, $format);
+
+        //create all filter images
+        /** @var FilterManager $filterManager */
+        $filterManager = $this->container->get('liip_imagine.filter.manager');
+
+        //get all image filters
+        $filters = $filterManager->getFilterConfiguration()->all();
+        $filters = array_keys($filters);
+
+        unset($filters[0], $filters[1]);
+
+        //create cache versions for images
+        foreach ($filters as $key => $filter) {
+            $data = $filterManager->applyFilter($binary, $filter)->getContent();
+            if ($data) {
+                $base64 = 'data:image/' . $format . ';base64,' . base64_encode($data);
+
+                if ($key === 2) {
+                    $s3Id3535 = $image->getId() . '_35_35.' . $format;
+                    $image->setS3Id3535($s3Id3535);
+
+                    $this->s3Service->uploadFile($base64, $s3Id3535, $image->getType(), $image->getMimeType());
+                } elseif ($key === 3) {
+                    $s3Id150150 = $image->getId() . '_150_150.' . $format;
+                    $image->setS3Id150150($s3Id150150);
+
+                    $this->s3Service->uploadFile($base64, $s3Id150150, $image->getType(), $image->getMimeType());
+                } elseif ($key === 4) {
+                    $s3Id300300 = $image->getId() . '_300_300.' . $format;
+                    $image->setS3Id300300($s3Id300300);
+
+                    $this->s3Service->uploadFile($base64, $s3Id300300, $image->getType(), $image->getMimeType());
+                }
+            }
+        }
+
+        $this->em->persist($image);
     }
 }
